89a94f0c7822e47323ed0f502b2dd37a31c400e0
Sai Aryan Goswami <111999256+SaiAryan1784@users.noreply.github.com>
2023-06-07 23:27:17 +0530

    in process

---
+++ src/app.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from './components/header/header'
+import About from './components/about/about'
+import Experience from './components/experience/experience'
+import Contact from './components/contact/contact'
+import Services from './components/services/services'
+import Portfolio from './components/portfolio/portfolio'
+import Testimonials from './components/testimonials/testimonials'
+import Footer from './components/footer/footer'
+import Nav from './components/nav/nav'
+
+const app = () => {
+  return (
+    <>
+    <Header />
+    <Nav />
+    < About/>
+    < Experience/>
+    < Services/>
+    <Portfolio/>
+    < Testimonials/>
+    < Contact/>
+    < Footer/>
+    </>
+  )
+}
+
+export default app

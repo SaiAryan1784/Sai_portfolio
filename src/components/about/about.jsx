a77e15e2c1bb9183bfcadc53116198686eddfa51
Sai Aryan Goswami <111999256+SaiAryan1784@users.noreply.github.com>
2023-06-10 01:31:01 +0530

    About completed

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/dp22.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const about = () => {
   return (
@@ -10,12 +14,36 @@ const about = () => {
       <div className="container about__container">
         <div className="about__me">
           <div className="about__me-image">
-            <img src="" alt="" />
+            <img src={ME} alt="About Image" />
           </div>
         </div>
 
         <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Experience</h5>
+              <small>6 Months Working </small>
+            </article>
 
+            <article className="about__card">
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>2 - Ongoing :-| </small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>4+ Completed :'\ </small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus magnam eum omnis neque facilis labore modi quo quis, dicta, excepturi assumenda voluptatum deleniti porro quibusdam magni? Exercitationem dolorem adipisci pariatur fugiat temporibus, explicabo soluta?
+          </p>
+
+          <a href="#contact" className='btn btn-primary'> Let's Talk</a>
         </div>
       </div>
     </section>
--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,9 +1,102 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const experience = () => {
   return (
-    <section id='experience'>experience</section>
+    <section id='experience'>
+      <h5>The Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+
+        <div className="experience__frontend">
+
+          <h3>Frontend Development</h3>
+
+          <div className="experience__content">
+            <article className='experience__details'>
+
+              <BsPatchCheckFill/>
+              <h4>HTML</h4>
+              <small className='text-light'>Experienced</small>
+
+            </article>
+            <article className='experience__details'>
+
+              <BsPatchCheckFill/>
+              <h4>CSS</h4>
+              <small className='text-light'>Intermediate</small>
+
+            </article>
+            <article className='experience__details'>
+
+              <BsPatchCheckFill/>
+              <h4>JavaScript</h4>
+              <small className='text-light'>Beginner</small>
+
+            </article>
+            <article className='experience__details'>
+
+              <BsPatchCheckFill/>
+              <h4>React</h4>
+              <small className='text-light'>Beginner</small>
+
+            </article>
+            <article className='experience__details'>
+
+              <BsPatchCheckFill/>
+              <h4>Tailwind</h4>
+              <small className='text-light'>Beginner</small>
+
+            </article>
+          </div>
+        </div>
+{/* ==================frontend end====================== */}
+        <div className="experience__cp">
+          
+          <h3>Competitive Programming</h3>
+
+          <div className="experience__content">
+            <article className='experience__details'>
+
+              <BsPatchCheckFill/>
+              <h4>C</h4>
+              <small className='text-light'>Experienced</small>
+
+            </article>
+            <article className='experience__details'>
+
+              <BsPatchCheckFill/>
+              <h4>C++</h4>
+              <small className='text-light'>Intermediate</small>
+
+            </article>
+            <article className='experience__details'>
+
+              <BsPatchCheckFill/>
+              <h4>Python</h4>
+              <small className='text-light'>Intermediate</small>
+
+            </article>
+            <article className='experience__details'>
+
+              <BsPatchCheckFill/>
+              <h4>Python-Numpy</h4>
+              <small className='text-light'>Intermediate</small>
+
+            </article>
+            <article className='experience__details'>
+
+              <BsPatchCheckFill/>
+              <h4>Python-Pandas</h4>
+              <small className='text-light'>Beginner</small>
+
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 

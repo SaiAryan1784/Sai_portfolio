52794be0445d9a39e1a4a338a32ff8e38d4a8386
Sai Aryan Goswami <111999256+SaiAryan1784@users.noreply.github.com>
2023-06-11 20:52:22 +0530

    portfolio created

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,5 +1,40 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Project 1',
+    github: "https://github.com/SaiAryan1784/Sai_portfolio",
+    demo: "https://github.com/SaiAryan1784/Sai_portfolio"
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Project 2',
+    github: "https://github.com/SaiAryan1784/Sai_portfolio",
+    demo: "https://github.com/SaiAryan1784/Sai_portfolio"
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Project 3',
+    github: "https://github.com/SaiAryan1784/Sai_portfolio",
+    demo: "https://github.com/SaiAryan1784/Sai_portfolio"
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Project 4',
+    github: "https://github.com/SaiAryan1784/Sai_portfolio",
+    demo: "https://github.com/SaiAryan1784/Sai_portfolio"
+  }
+]
 
 const portfolio = () => {
   return (
@@ -10,15 +45,27 @@ const portfolio = () => {
 
       <div className="container portfolio__container">
 
-        <article className="portfolio__item">
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return(
+              <article key={id} className="portfolio__item">
 
-          <div className="portfolio__item-image"></div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com/SaiAryan1784/Sai_portfolio" className='btn'>Github</a>
-          <a href="https://github.com/SaiAryan1784/Sai_portfolio" className='btn'>Github</a>
+          <div className="portfolio__item-image">
+            <img src={image} alt={title} />
+          </div>
+          
+          <h3>{title}</h3>
+
+          <div className="portfolio item-cta">
+          <a href={github} className='btn' target='_blank'>Github</a>
+          <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
 
         </article>
-
+            )
+          })
+        }
+        
       </div>
 
     </section>
--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar01.jpg'
+import AVTR2 from '../../assets/avatar02.jpg'
+import AVTR3 from '../../assets/avatar03.jpg'
+import AVTR4 from '../../assets/avatar04.jpg'
 
 const testimonials = () => {
   return (
-    <section id='testimonials'>testimonials</section>
+    <section id='testimonials'>
+      <h5>Review from clients - Be the first one</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="avatar 01" />
+            <h5 className='client__name'>THE ROCK</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aperiam vel aspernatur, explicabo consequuntur eos numquam fugit ullam. Eius esse incidunt officiis cum.
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="avatar 01" />
+            <h5 className='client__name'>THE ROCK</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aperiam vel aspernatur, explicabo consequuntur eos numquam fugit ullam. Eius esse incidunt officiis cum.
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="avatar 01" />
+            <h5 className='client__name'>THE ROCK</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aperiam vel aspernatur, explicabo consequuntur eos numquam fugit ullam. Eius esse incidunt officiis cum.
+            </small>
+          </div>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVTR1} alt="avatar 01" />
+            <h5 className='client__name'>THE ROCK</h5>
+            <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aperiam vel aspernatur, explicabo consequuntur eos numquam fugit ullam. Eius esse incidunt officiis cum.
+            </small>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
